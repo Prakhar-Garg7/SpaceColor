96d7b8d2a99878affb37da72f980536ed1532b88
Prakhar <prakhargarg1405@gmail.com>
2024-05-24 22:49:31 +0530

    starting project

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,5 +1,6 @@
 # I created this file - Prakhar
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-                    return HttpResponse("hello world")
+                    return render(request, 'index.html')
