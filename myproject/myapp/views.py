d32b7db10a71c57481030578eb79d1f668e80f74
Prakhar <prakhargarg1405@gmail.com>
2024-05-27 17:14:34 +0530

    creatyed register page

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import UserAuthData
 
 # Create your views here.
 def index ( request ) :
                                return render( request, index.html )       
 
 def gradient ( request ) :
-                               return render( request, 'gradient.html' )       
+                                        newUser = UserAuthData(
+                                        username=request.GET.get('username'),
+                                        email=request.GET.get('email'),
+                                        password=request.GET.get('password')
+)
+                                        newUser.save()
+                                        return render( request, 'gradient.html' )       
 
 def register ( request ) :
                                return render( request, 'register.html' )  
