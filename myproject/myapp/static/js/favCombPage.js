d37d826de1459c2c054d9957e394a40e66b5d701
Prakhar <prakhargarg1405@gmail.com>
2024-05-28 21:41:23 +0530

    added favComb Page

--- myproject/myapp/static/js/favCombPage.js
+++ myproject/myapp/static/js/favCombPage.js
@@ -1,22 +1,40 @@
-// console.log("hello");
-// const queryString = window.location.search;
-// const urlParams = new URLSearchParams(queryString);
+var cont = document.getElementsByClassName('cont')[0];
 
-// console.log("Query String:", queryString);
-// console.log("URL Params:", urlParams);
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
 
-// const dataParam = urlParams.get("data");
-// if (dataParam !== null) {
-//     const decodedDataParam = decodeURIComponent(dataParam);
-//     console.log("Data Parameter Value (Decoded):", decodedDataParam);
-//     const dataArray = JSON.parse(decodedDataParam);
-//     console.log("length of list is: ", dataArray.length) ;
-//     for ( let i = 0; i < 2; i++ ) {
+const dataParam = urlParams.get("data");
+const decodedDataParam = decodeURIComponent(dataParam);
+const dataArray = JSON.parse(decodedDataParam);
 
-//         console.log( dataArray[i].first_col ) ;
-//         console.log( dataArray[i].sec_col ) ;
-//         console.log( dataArray[i].angle ) ;
-//     }
-// } else {
-//     console.log("Data Parameter Not Found");
-// }
+for (let i = 0; i < dataArray.length; i++) {
+                                        let divi = document.createElement('div');
+                                        divi.style.background = 'linear-gradient(' + dataArray[i].angle + 'deg' + ', ' + dataArray[i].first_col + ', ' + dataArray[i].sec_col + ')' ;
+                                        
+                                        // Create the span elements
+                                        let span1 = document.createElement('span');
+                                        let span2 = document.createElement('span');
+                                        let span3 = document.createElement('span');
+                                        
+                                        // Set the inner HTML for each span
+                                        span1.innerHTML = 'Color1 : ' + dataArray[i].first_col;
+                                        span2.innerHTML = 'Color2 : ' + dataArray[i].sec_col;
+                                        span3.innerHTML = 'Angle : ' + dataArray[i].angle;
+                                        
+                                        // Add class to the spans
+                                        span1.classList.add('itemcont');
+                                        span2.classList.add('itemcont');
+                                        span3.classList.add('itemcont');
+                                        
+                                        // Append spans to the div
+                                        divi.appendChild(span1);
+                                        divi.appendChild(span2);
+                                        divi.appendChild(span3);
+                                        
+                                        // Add class to the div
+                                        divi.classList.add('item');
+                                        
+                                        // Append the div to the container
+                                        cont.appendChild(divi);
+}
+                                    
